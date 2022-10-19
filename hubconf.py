a978781315279ca89fb5ed75f57ff68feee446a1
Bobba Ruthvik <73455820+cs19b007iittp@users.noreply.github.com>
2022-10-19 09:43:39 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,29 +1,196 @@
 import torch
 from torch import nn
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import LogSoftmax
+from torch import flatten
 
 def kali():
   print ('kali')
+
+
+
+
+
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+
+
+
+
   
 # Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
-class YourRollNumberNN(nn.Module):
+class cs19b007NN(nn.Module):
   pass
   # ... your code ...
   # ... write init and forward functions appropriately ...
+  def __init__(self):
+      super().__init__()
+      self.flatten = nn.Flatten()
+      self.linear_relu_stack = nn.Sequential(
+          nn.Linear(28*28, 512),                                                                                  #check its input size
+          nn.ReLU(),
+          nn.Linear(512, 512),
+          nn.ReLU(),
+          nn.Linear(512, 10)                                                                                      # how do we get number of classes
+      )
+
+  def forward(self, x):
+      x = self.flatten(x)
+      logits = self.linear_relu_stack(x)
+      return logits
+
+model = cs19b007NN().to(device)
+print(model)
     
+
+
+
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
+
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+
+
+
+
+
+# def test(dataloader, model, loss_fn):
+#     size = len(dataloader.dataset)
+#     num_batches = len(dataloader)
+#     model.eval()
+#     test_loss, correct = 0, 0
+#     with torch.no_grad():
+#         for X, y in dataloader:
+#             X, y = X.to(device), y.to(device)
+#             pred = model(X)
+#             test_loss += loss_fn(pred, y).item()
+#             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+#     test_loss /= num_batches
+#     correct /= size
+#     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+
+
+
+
+
+
+
+
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
   model = None
-
   # write your code here as per instructions
   # ... your code ...
   # ... your code ...
   # ... and so on ...
   # Use softmax and cross entropy loss functions
   # set model variable to proper object, make use of train_data
-  
+
+  model = cs19b007NN().to(device)
+  for t in range(n_epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_data_loader, model, loss_fn, optimizer)
+    # test(test_dataloader, model, loss_fn)
+  # print("Done!")
   print ('Returning model... (rollnumber: xx)')
   
   return model
+
+
+
+
+
+
+
+
+class LeNet(Module):
+  def __init__(self, numChannels, classes):
+    # call the parent constructor
+    super(LeNet, self).__init__()
+    # initialize first set of CONV => RELU => POOL layers
+    self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+      kernel_size=(5, 5))
+    self.relu1 = ReLU()
+    self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+    # initialize second set of CONV => RELU => POOL layers
+    self.conv2 = Conv2d(in_channels=20, out_channels=50,
+      kernel_size=(5, 5))
+    self.relu2 = ReLU()
+    self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+    # initialize first (and only) set of FC => RELU layers
+    self.fc1 = Linear(in_features=800, out_features=500)
+    self.relu3 = ReLU()
+    # initialize our softmax classifier
+    self.fc2 = Linear(in_features=500, out_features=classes)
+    self.logSoftmax = LogSoftmax(dim=1)
+
+    
+  def forward(self, x):
+    # pass the input through our first set of CONV => RELU =>
+    # POOL layers
+    x = self.conv1(x)
+    x = self.relu1(x)
+    x = self.maxpool1(x)
+    # pass the output from the previous layer through the second
+    # set of CONV => RELU => POOL layers
+    x = self.conv2(x)
+    x = self.relu2(x)
+    x = self.maxpool2(x)
+    # flatten the output from the previous layer and pass it
+    # through our only set of FC => RELU layers
+    x = flatten(x, 1)
+    x = self.fc1(x)
+    x = self.relu3(x)
+    # pass the output to our softmax classifier to get our output
+    # predictions
+    x = self.fc2(x)
+    output = self.logSoftmax(x)
+    # return the output predictions
+    return output
+
+
+
+
+
+
+
+
+
 
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
@@ -45,12 +212,34 @@ def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
   
   # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
   # HINT: Flatten function can also be used if required
+  # return model
+
+  in_channels = config[0]
+  out_channels = config[1] 
+  kernel_size = config[2] 
+  stride= config[3]
+  padding=config[4]
+
+  model = cs19b007NN().to(device)
+  for t in range(n_epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_data_loader, model, loss_fn, optimizer)
+    # test(test_dataloader, model, loss_fn)
+  # print("Done!")
+  print ('Returning model... (rollnumber: xx)')
+  
   return model
   
   
   print ('Returning model... (rollnumber: xx)')
   
   return model
+
+
+
+
+
+
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 def test_model(model1=None, test_data_loader=None):
@@ -65,5 +254,3 @@ def test_model(model1=None, test_data_loader=None):
   print ('Returning metrics... (rollnumber: xx)')
   
   return accuracy_val, precision_val, recall_val, f1score_val
-
-
